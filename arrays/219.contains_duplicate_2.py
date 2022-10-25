75ed2d2e19a22d995b49c92cd7678d524362646a
educee <112855710+educee@users.noreply.github.com>
2022-10-25 06:29:21 +0530

    Create 219.contains_duplicate_2.py

---
+++ arrays/219.contains_duplicate_2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hashmap = {}
+        
+        for idx, val in enumerate(nums):
+            if val in hashmap.keys() and (idx - hashmap[val]) <= k:
+                return True
+            
+            hashmap[val] = idx
+        return False
