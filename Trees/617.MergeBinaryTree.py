e8b173956eee8268f9473c3ad740ab60f785f3d0
educee <112855710+educee@users.noreply.github.com>
2022-11-03 20:09:20 +0530

    Create 617.MergeBinaryTree.py

---
+++ Trees/617.MergeBinaryTree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        if root1 and root2:
+            newVal = root1.val + root2.val
+            newNode = TreeNode(newVal)
+            newNode.left = self.mergeTrees(root1.left, root2.left)
+            newNode.right = self.mergeTrees(root1.right, root2.right)
+        else:
+            return root1 or root2
+        
+        
+        return newNode 
